c2dd765b45fcc0111da16af473aa57b9dded354a
queen <Queenmoncler@gmail.com>
2023-12-18 21:49:16 +0300

    Initial commit

---
+++ src/main/java/com/example/netology_springbootjdbc/NetologySpringBootJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.netology_springbootjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetologySpringBootJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetologySpringBootJdbcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/netology_springbootjdbc/NetologySpringBootJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.netology_springbootjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetologySpringBootJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
